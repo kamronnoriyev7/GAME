de8b2a1c9adfdfeebad1f2db1a12d973290f05e4
kamronnoriyev <kamronnoriyev7@gmail.com>
2024-11-20 20:55:19 +0500

    .

--- Game.cs
+++ Game.cs
@@ -1,15 +1,15 @@
 
 public class Game
 {
-    public string Title { get; set; } // O'yin nomi
+    public string Title { get; set; } 
     
-    public string Genre { get; set; } // Janr
+    public string Genre { get; set; } 
     
-    public string Developer { get; set; } // Dasturchi kompaniya
+    public string Developer { get; set; } 
     
-    public string Publisher { get; set; } // Nashriyot
+    public string Publisher { get; set; } 
     
-    public int ReleaseYear { get; set; } // Chiqish yili
+    public int ReleaseYear { get; set; }
     
     public string Platform { get; set; } 
     
